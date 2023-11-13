85a60446d997c76227b02340d5499ad31d9d0dcc
RazvanGP <113028458+RazvanGP@users.noreply.github.com>
2023-11-13 19:29:48 +0200

    update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,3 @@
-import HeroSection from "./components/HeroSection";
-
 export default function Home() {
-  return (
-    <div className="h-[200vh]">
-      <HeroSection />
-    </div>
-  );
+  return <div className="h-[200vh]"></div>;
 }
