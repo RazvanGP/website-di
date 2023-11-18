221f71ac7b0f2da7f7230d6e086b2503082b666f
RazvanGP <113028458+RazvanGP@users.noreply.github.com>
2023-11-19 00:09:58 +0200

    hero update

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -1,6 +1,8 @@
 import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 import { Context } from "./Context";
 import { useContext, useState } from "react";
+import Link from "next/link";
+
 const HeroSection = () => {
   const [scrollYSection, setScrollYSection] = useState(0);
   const { scrollY } = useScroll();
@@ -10,22 +12,27 @@ const HeroSection = () => {
   });
 
   return (
-    <section className="relative h-[320vh] ">
+    <section className="relative h-[320vh]">
       <div className="sticky top-0 flex h-screen items-center overflow-hidden">
-        <motion.div
-          className="flex gap-4"
-          animate={{ x: -scrollYSection }}
-          transition={{ type: "spring", stiffness: 30 }}
-        >
-          <div className="h-[450px] min-w-max  font-bold pt-28 font-primary pl-10">
-            <h1 className="text-3xl text-accent-red font-bold tracking-widest sticky left-0 translate-x-0">
-              SHIP DESIGN ENGINEERING
-            </h1>
-            <p className="text-[10rem] text-primary-blue font-black opacity-10">
-              WHERE VISION SETS SAIL
-            </p>
-          </div>
-        </motion.div>
+        {/* bg-gradient-to-t from-[rgba(0,110,255,0.001)] via-[rgba(0,110,255,0.1)] to-[rgba(0,110,255,0.001)] */}
+        <div className=" p-10 mt-48 min-w-max font-bold font-primary pl-5 flex flex-col items-start">
+          <h1 className="text-sm sm:text-xl text-accent-red font-extrabold tracking-[5px] sm:tracking-[10px] drop-shadow-xl">
+            SHIP DESIGN ENGINEERING
+          </h1>
+          <motion.p
+            className="text-9xl text-primary-blue font-black opacity-10 "
+            animate={{ x: -scrollYSection }}
+            transition={{ type: "spring", stiffness: 30 }}
+          >
+            WHERE VISION SETS SAIL
+          </motion.p>
+          <Link
+            href="/services"
+            class="mt-24 px-4 py-2 text-primary-blue uppercase bg-transparent border-2 border-primary-blue hover:bg-primary-blue hover:text-white text-md duration-1000"
+          >
+            explore our services
+          </Link>
+        </div>
       </div>
     </section>
   );
--- app/page.js
+++ app/page.js
@@ -1,35 +1,17 @@
 "use client";
-import { motion, AnimatePresence, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState, useContext } from "react";
 import { Context } from "./components/Context";
 import HeroSection from "./components/HeroSection";
 import WelcomeSection from "./components/WelcomeSection";
-
-const homeSectionsVariants = {
-  hidden: {
-    opacity: 0,
-    y: "-100vh",
-  },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      type: "spring",
-      bounce: 0.4,
-      duration: 0.8,
-    },
-  },
-};
 
 export default function Home() {
   const { scrollYValue } = useContext(Context);
 
   return (
     <div>
-      <AnimatePresence>
-        <HeroSection />
-        <section className="min-h-screen bg-red-200"></section>c
-      </AnimatePresence>
+      <HeroSection />
+      <section className="min-h-screen bg-red-200"></section>c
     </div>
   );
 }
